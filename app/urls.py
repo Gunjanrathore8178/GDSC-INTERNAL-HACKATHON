4ade1c8eb2a91090015b7c4ab5b27af0cdd34fec
Jatin Singh <sagoisinghjatin9951@gmail.com>
2022-10-28 19:20:48 +0530

    Backend

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('map', views.map, name='map'),
-    path('pay', views.pay, name='pay'),
-    path('widget', views.widget, name='widget'),
-    path('newsletter', views.newsletter, name='newsletter'),
-    path('globe', views.globe, name='globe')
+    path('map/', views.map, name='map'),
+    path('pay/', views.pay, name='pay'),
+    path('widget/', views.widget, name='widget'),
+    path('newsletter/', views.newsletter, name='newsletter'),
+    path('globe/', views.globe, name='globe')
 ]
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 // vite.config.js
-import glsl from 'vite-plugin-glsl';
-import { defineConfig } from 'vite';
+import glsl from "vite-plugin-glsl";
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [glsl()]
+  plugins: [glsl()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "templates/index.html"),
+      },
+    },
+  },
 });
